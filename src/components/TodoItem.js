12515eac9f74ecc12879d696bb87f2a169c55bb7
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-12 04:49:58 +0200

    Add To-dos components files: src/components/Header.js, src/components/InputTodo.js, src/components/Navbar.js, src/components/TodoContainer.js. , src/components/TodoItem.js and src/components/TodosList.js

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <li>{this.props.todo.title}</li>
+    );
+  }
+}
+
+export default TodoItem;
---
+++ src/components/TodosList.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+class TodosList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodosList;
