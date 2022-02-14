f87f688437012b5740354d63f324a4a42894880f
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-14 10:45:10 +0200

    Rename src/scss/{index.scss => App.scss} and Add some styling on the UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/index.scss';
+import './scss/App.scss';
 // component file
 import TodoContainer from './components/TodoContainer';
 
