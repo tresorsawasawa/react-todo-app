c815124048692bbd8217f4b38727f1ea1fcf3234
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-14 10:14:01 +0200

    Implement functionalaities to add new todo, delete a todo and check if completed

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,13 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable no-param-reassign */
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable no-unused-labels */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
+// import { v4 as uuidv4 } from 'uuid';
 import TodosList from './TodosList';
+import Header from './Header';
+import InputTodo from './InputTodo';
 
 class TodoContainer extends React.Component {
   constructor(props) {
@@ -10,7 +17,7 @@ class TodoContainer extends React.Component {
         {
           id: 1,
           title: 'Setup development environment',
-          completed: true,
+          completed: false,
         },
         {
           id: 2,
@@ -26,11 +33,54 @@ class TodoContainer extends React.Component {
     };
   }
 
+  handleChangeCompleted = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  }
+
+  deleteTodo = (id) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter((todo) => todo.id !== id),
+      ],
+    });
+  }
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  }
+
   render() {
     return (
-      <div>
-        <TodosList todos={this.state.todos} />
-      </div>
+      <>
+        <Header />
+        <React.StrictMode>
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <div>
+            <TodosList
+              todos={this.state.todos}
+              handleChangeProps={this.handleChangeCompleted}
+              deleteTodoProps={this.deleteTodo}
+            />
+          </div>
+        </React.StrictMode>
+      </>
     );
   }
 }
