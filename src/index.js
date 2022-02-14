960ec32ee81e6247f59cfaccf7742d5739294bd5
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-14 21:36:53 +0200

    Remove all unused files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './functionBased/scss/App.scss';
+import './scss/App.scss';
 // component file
-import TodoContainer from './functionBased/components/TodoContainer';
+import TodoContainer from './components/TodoContainer';
 
 ReactDOM.render(
   <React.StrictMode>
